e40173df40e2b68cbf12b9be796f927830dddda2
tl11bi <tl11bi@users.noreply.github.com>
2021-05-19 16:00:46 -0400

    adding more interface

---
+++ src/List/Hashtable.java
@@ -0,0 +1,11 @@
+package List;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class Hashtable<K, V> {
+
+
+
+
+}
--- src/List/Queue.java
+++ src/List/Queue.java
@@ -1,6 +1,26 @@
 package List;
 
-public interface Queue<E> extends Iterable, Collection<E>{
+public interface Queue<E> extends Iterable, Collection<E> {
 
+
+    /*    Retrieves, but does not remove, the head of this queue.
+     */
+    public E element();
+
+    /*    Inserts the specified element into this queue if it is possible to do so immediately without violating capacity restrictions.
+     */
+    public boolean offer(E e);
+
+    /*    Retrieves, but does not remove, the head of this queue, or returns null if this queue is empty.
+     */
+    public E peek();
+
+    /*    Retrieves and removes the head of this queue, or returns null if this queue is empty.
+     */
+    public E poll();
+
+    /*    Retrieves and removes the head of this queue.
+     */
+    public E remove();
 
 }
